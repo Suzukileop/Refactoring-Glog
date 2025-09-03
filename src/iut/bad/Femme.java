15d6d8048d18b31d11a4f9bff933e4138195dd2e
Suzukileop <leopardjuliocesar8@gmail.com>
2025-09-04 01:17:07 +0300

    Ajout d'une méthode main dans la classe femme avec deux instances.

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -6,4 +6,13 @@ public class Femme extends Humain {
     public Femme(String nom, String prenom, int age) {
         super(nom, prenom, age);
     }
+    
+    
+    public static void main(String[] args) {
+        Homme homme = new Homme("Dupont", "Jean", 30);
+        Femme femme = new Femme("Martin", "Marie", 28);
+        
+        homme.ami(femme);
+        femme.ami(homme);
+    }
 }
